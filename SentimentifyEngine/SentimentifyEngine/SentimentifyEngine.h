3e2b1409c3b96485aba3749a9880bb028df746c7
Thales Frigo <thalesfrigo@gmail.com>
2021-04-21 16:21:22 -0300

    Setup Workspace

---
+++ SentimentifyEngine/SentimentifyEngine/SentimentifyEngine.h
@@ -0,0 +1,18 @@
+//
+//  SentimentifyEngine.h
+//  SentimentifyEngine
+//
+//  Created by Thales Frigo on 21/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SentimentifyEngine.
+FOUNDATION_EXPORT double SentimentifyEngineVersionNumber;
+
+//! Project version string for SentimentifyEngine.
+FOUNDATION_EXPORT const unsigned char SentimentifyEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SentimentifyEngine/PublicHeader.h>
+
+
