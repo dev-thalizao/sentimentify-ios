ec41d14725ef18d1658ff36e96e20d22138a9e36
Thales Frigo <thalesfrigo@gmail.com>
2021-04-24 09:37:39 -0300

    Create specific infrastructure modules using Google and Twitter products.

---
+++ SentimentifyEngine/GoogleAnalyzeInfrastructure/GoogleAnalyzeInfrastructure.h
@@ -0,0 +1,18 @@
+//
+//  GoogleAnalyzeInfrastructure.h
+//  GoogleAnalyzeInfrastructure
+//
+//  Created by Thales Frigo on 24/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleAnalyzeInfrastructure.
+FOUNDATION_EXPORT double GoogleAnalyzeInfrastructureVersionNumber;
+
+//! Project version string for GoogleAnalyzeInfrastructure.
+FOUNDATION_EXPORT const unsigned char GoogleAnalyzeInfrastructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleAnalyzeInfrastructure/PublicHeader.h>
+
+
