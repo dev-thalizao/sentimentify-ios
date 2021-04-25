89919ab14fb6de82700d942791ad5aa524c025ad
Thales Frigo <thalesfrigo@gmail.com>
2021-04-25 09:50:58 -0300

    Create SentimentifyiOS module.

---
+++ SentimentifyEngine/SentimentifyiOS/SentimentifyiOS.h
@@ -0,0 +1,18 @@
+//
+//  SentimentifyiOS.h
+//  SentimentifyiOS
+//
+//  Created by Thales Frigo on 25/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SentimentifyiOS.
+FOUNDATION_EXPORT double SentimentifyiOSVersionNumber;
+
+//! Project version string for SentimentifyiOS.
+FOUNDATION_EXPORT const unsigned char SentimentifyiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SentimentifyiOS/PublicHeader.h>
+
+
